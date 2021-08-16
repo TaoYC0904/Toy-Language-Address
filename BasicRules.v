9bafc84e1f561bd0622b17d80e299dfb3931b468
TaoYC0904 <taoyc0904@sjtu.edu.cn>
2021-08-16 22:31:28 +0800

    soundness of basic rules proved

--- BasicRules.v
+++ BasicRules.v
@@ -250,4 +250,189 @@ Proof.
   split; simpl; tauto.
 Qed.
 
+Lemma aeval_join1 : forall e st1 st2 st v,
+  stateJ st1 st2 st ->
+  aeval e st1 = Some v ->
+  aeval e st = Some v.
+Proof.
+  intros.
+  UFjoin.
+  simpl in *. destruct H as [[? ?] ?].
+  UFheapjoin.
+  rewrite osajoin in H2.
+  revert v H0; induction e.
+  + simpl. tauto.
+  + simpl in *. rewrite <-H. tauto.
+  + simpl in *. intros. unfold add_sem in *.
+    destruct (aeval e1 st1), (aeval e2 st1); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H5. tauto.
+  + simpl in *. intros. unfold sub_sem in *.
+    destruct (aeval e1 st1), (aeval e2 st1); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H5. tauto.
+  + simpl in *. intros. unfold mul_sem in *.
+    destruct (aeval e1 st1), (aeval e2 st1); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H5. tauto.
+  + simpl in *. intros. unfold div_sem in *.
+    destruct (aeval e1 st1), (aeval e2 st1); try inversion H0.
+    destruct (Z.eq_dec z0 0); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H6. destruct (Z.eq_dec z0 0); tauto.
+  + simpl in *. intros. unfold deref_sem in *.
+    destruct (aeval e st1); try inversion H0.
+    rename z into p. specialize (IHe p).
+    assert (aeval e st = Some p). { tauto. }
+    clear H4.
+    specialize (H2 p). destruct H2 as [? | [? | ?]].
+    - destruct H2 as [v0 [? [? ?]]].
+      rewrite H2 in H0. inversion H0. subst v0.
+      rewrite H3, H5, H2. tauto.
+    - destruct H2 as [v0 [? [? ?]]].
+      rewrite H2 in H0. inversion H0.
+    - destruct H2 as [? [? ?]].
+      rewrite H2 in H0. inversion H0.
+Qed.
 
+Lemma aeval_join2 : forall e st1 st2 st v,
+  stateJ st1 st2 st ->
+  aeval e st2 = Some v ->
+  aeval e st = Some v.
+Proof.
+  intros.
+  UFjoin.
+  simpl in *. destruct H as [[? ?] ?].
+  UFheapjoin.
+  rewrite osajoin in H2.
+  revert v H0; induction e.
+  + simpl. tauto.
+  + simpl in *. rewrite <-H1. tauto.
+  + simpl in *. intros. unfold add_sem in *.
+    destruct (aeval e1 st2), (aeval e2 st2); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H5. tauto.
+  + simpl in *. intros. unfold sub_sem in *.
+    destruct (aeval e1 st2), (aeval e2 st2); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H5. tauto.
+  + simpl in *. intros. unfold mul_sem in *.
+    destruct (aeval e1 st2), (aeval e2 st2); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H5. tauto.
+  + simpl in *. intros. unfold div_sem in *.
+    destruct (aeval e1 st2), (aeval e2 st2); try inversion H0.
+    destruct (Z.eq_dec z0 0); try inversion H0.
+    specialize (IHe1 z). specialize (IHe2 z0).
+    assert (aeval e1 st = Some z). { tauto. }
+    assert (aeval e2 st = Some z0). { tauto. }
+    rewrite H3, H6. destruct (Z.eq_dec z0 0); tauto.
+  + simpl in *. intros. unfold deref_sem in *.
+    destruct (aeval e st2); try inversion H0.
+    rename z into p. specialize (IHe p).
+    assert (aeval e st = Some p). { tauto. }
+    clear H4.
+    specialize (H2 p). destruct H2 as [? | [? | ?]].
+    - destruct H2 as [v0 [? [? ?]]].
+      rewrite H4 in H0. inversion H0.
+    - destruct H2 as [v0 [? [? ?]]].
+      rewrite H4 in H0. inversion H0. subst v0.
+      rewrite H3, H5, H4. tauto.
+    - destruct H2 as [? [? ?]].
+      rewrite H4 in H0. inversion H0.
+Qed.
+
+Theorem hoare_store_sound : forall P e1 e2 p v,
+  derives P (andp (eqp e1 p) (eqp e2 v)) ->
+  valid (sepcon P (mapsto_ p)) (CAss_store e1 e2) (sepcon P (mapsto p v)) falsep falsep.
+Proof.
+  unfold derives, valid; intros.
+  split.
+  { unfold not; intros.
+    UFsepcon. destruct H0 as [st11 [st12 [? [? ?]]]].
+    specialize (H st11 H2).
+    destruct H. unfold eqp in H, H4.
+    pose proof (aeval_join1 _ _ _ _ _ H0 H).
+    pose proof (aeval_join1 _ _ _ _ _ H0 H4).
+    simpl in *. rewrite H5, H6 in H1.
+    unfold mapsto_ in H3. destruct H3.
+    destruct H3 as [v0 ?].
+    UFjoin. destruct H0.
+    UFheapjoin. rewrite osajoin in H8.
+    specialize (H8 p).
+    destruct H8 as [? | [? | ?]].
+    + destruct H8 as [? [? [? ?]]].
+      rewrite H9 in H3. inversion H3.
+    + destruct H8 as [? [? [? ?]]].
+      rewrite H10 in H1. inversion H1.
+    + destruct H8 as [? [? ?]].
+      rewrite H9 in H3. inversion H3. }
+  split.
+  { intros.
+    UFsepcon. destruct H0 as [st11 [st12 [? [? ?]]]].
+    remember (fst st1, heap_update (snd st12) p (Some v)) as st22.
+    exists st11, st22.
+    specialize (H st11 H2).
+    destruct H. unfold eqp in *.
+    pose proof (aeval_join1 _ _ _ _ _ H0 H).
+    pose proof (aeval_join1 _ _ _ _ _ H0 H4).
+    UFjoin. destruct H0 as [[? ?] ?].
+    simpl in H1. rewrite H5, H6 in H1.
+    destruct H1 as [? [? [? ?]]].
+    split; split; try split.
+    + rewrite H0, H11; tauto.
+    + subst st22. unfold fst at 1. tauto.
+    + UFheapjoin. rewrite osajoin in *.
+      intros p0. specialize (H8 p0).
+      destruct H8 as [? | [? | ?]].
+      - left.
+        destruct H8 as [v0 [? [? ?]]].
+        subst st22. unfold heap_update. unfold snd at 2.
+        destruct (Z.eq_dec p p0).
+        * subst p0. unfold mapsto_ in H3.
+          destruct H3 as [[v' ?] ?].
+          rewrite H3 in H12. inversion H12.
+        * exists v0.
+          specialize (H10 p0 n). rewrite H13 in H10.
+          tauto. 
+      - right. left.
+        destruct H8 as [v0 [? [? ?]]].
+        subst st22. unfold heap_update. unfold snd at 2.
+        destruct (Z.eq_dec p p0).
+        * exists v. subst p0. tauto.
+        * exists v0. 
+          specialize (H10 p0 n). rewrite H10. tauto.
+      - right. right.
+        destruct H8 as [? [? ?]].
+        unfold mapsto_ in H3.
+        destruct H3 as [[v' ?] ?].
+        destruct (Z.eq_dec p p0).
+        * subst p0. rewrite H3 in H12; inversion H12.
+        * subst st22. unfold heap_update. unfold snd at 2.
+          destruct (Z.eq_dec p p0); try tauto.
+          specialize (H10 p0 n). rewrite H10. tauto.
+    + tauto.
+    + subst st22. unfold heap_update. unfold snd at 1.
+      destruct (Z.eq_dec p p); tauto.
+    + intros. subst st22. unfold heap_update. unfold snd at 1.
+      destruct (Z.eq_dec p p'); try tauto.
+      unfold mapsto_ in H3.
+      destruct H3. eapply H13. tauto. }
+  split; simpl; tauto.
+Qed.
+
+End BasicRulesSound.
