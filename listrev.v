78044fea2230615dfc4e966a64d229bdb80f8984
TaoYC0904 <taoyc0904@sjtu.edu.cn>
2021-08-20 12:23:24 +0800

    indentations changed

--- listrev.v
+++ listrev.v
@@ -11,8 +11,8 @@ Definition exp {A : Type} (P : A -> Assertion) : Assertion := fun st => exists a
 Definition NULL : addr := -1.
 
 Fixpoint listrep (p : Z) (l : list Z) (q1 q2 : Q) : Assertion :=
-	match l with
-		| nil => fun _ => p = NULL 
-		| cons x l' => sepcon (mapsto' p q1 x) (exp (fun t => sepcon (mapsto' (p + 1) q2 t) (listrep t l' q1 q2)))
-	end.
+  match l with
+	  | nil => fun _ => p = NULL 
+	  | cons x l' => sepcon (mapsto' p q1 x) (exp (fun t => sepcon (mapsto' (p + 1) q2 t) (listrep t l' q1 q2)))
+  end.
 	
